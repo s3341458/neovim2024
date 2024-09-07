28449334465257b242dd591c5d16aa07a2ac6856
chengyu <chengyu0316@gmail.com>
2024-09-08 09:02:33 +1000

    before setup cmp

--- lua/chengyu/core/keymaps.lua
+++ lua/chengyu/core/keymaps.lua
@@ -1,12 +1,12 @@
 -- set leader key to space
-vim.g.mapleader = " "
+vim.g.mapleader = ";"
 
 local keymap = vim.keymap -- for conciseness
 
 ---------------------
 -- General Keymaps -------------------
 
--- use jk to exit insert mode
+-- use keys to quickly exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("i", "kk", "<ESC>", { desc = "Exit insert mode with jk" })
@@ -28,14 +28,16 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
+--- something else
+
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<C-]>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<C-[>", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
